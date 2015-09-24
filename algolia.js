653df1c10c61a6ed61ffcf683628db892a7fb72d
Jessica Grinberg <jessgrinberg@gmail.com>
2015-09-24 14:57:31 -0700

    add requested fields for search

--- algolia.js
+++ algolia.js
@@ -1,13 +1,21 @@
 console.log("algolia script");
 
 
-function searchCallback(success, content) {
-    console.log(JSON.stringify(content));
-  };
+// function searchCallback(success, content) {
+//     console.log(JSON.stringify(content));
+//   };
+
+    function searchCallback(success, content) {
+      $('#users').empty();
+
+      for (var i = 0; i < content.hits.length; i++) {
+        $('#users').append('<li>' + content.hits[i].brandID + " , " +content.hits[i].objectID  + " ," +content.hits[i]._geoloc.lat  + " , " +content.hits[i]._geoloc.lng + '</li>');
+      }
+    };
 
   $(document).ready(function() {
     var algolia = new AlgoliaSearch('E4MMGYJF7T', 'd0ec88bf3074f5c26730541a7888d84a');
-    var index = algolia.initIndex('getstarted_actors');
+    var index = algolia.initIndex('test_NAME');
 
     $("input").keyup(function() {
       index.search($("input").val(), searchCallback, {
@@ -17,4 +25,3 @@ function searchCallback(success, content) {
     }).focus();
   });
 
-  
